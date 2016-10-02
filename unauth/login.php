b2397ca6f822b2f884c1b9255c4d8f593a687d20
rkanadam <raghuram.kanadam@gmail.com>
2016-10-02 00:34:29 -0700

    Initial registrar commit

--- unauth/login.php
+++ unauth/login.php
@@ -1,9 +1,31 @@
-<?php
-?>
+<!DOCTYPE html>
 <html>
-<head>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes">
+
+    <script src="../bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+    <link rel="import" href="../bower_components/paper-card/paper-card.html">
+    <style type="text/css">
+        body {
+            margin: 0;
+            font-family: 'Roboto'
+            font-size: 12px;
+        }
+    </style>
 </head>
 <body>
-Well! Got here :)
+
+<paper-card heading="Emmental" image="http://placehold.it/350x150/FFC107/000000" alt="Emmental">
+    <div class="card-content">
+        Emmentaler or Emmental is a yellow, medium-hard cheese that originated in the area around Emmental, Switzerland.
+        It is one of the cheeses of Switzerland, and is sometimes known as Swiss cheese.
+    </div>
+    <div class="card-actions">
+        <paper-button>Share</paper-button>
+        <paper-button>Explore!</paper-button>
+    </div>
+</paper-card>
 </body>
 </html>
