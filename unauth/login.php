78a672eff3e462bc1db2b9ca506d7598b3e5061c
rkanadam <raghuram.kanadam@gmail.com>
2016-10-02 00:38:06 -0700

    Adding initial login page

--- unauth/login.php
+++ unauth/login.php
@@ -7,6 +7,8 @@
 
     <script src="../bower_components/webcomponentsjs/webcomponents-lite.js"></script>
     <link rel="import" href="../bower_components/paper-card/paper-card.html">
+    <link rel="import" href="../bower_components/google-signin/google-signin.html">
+
     <style type="text/css">
         body {
             margin: 0;
@@ -17,14 +19,16 @@
 </head>
 <body>
 
-<paper-card heading="Emmental" image="http://placehold.it/350x150/FFC107/000000" alt="Emmental">
+<paper-card heading="Sign in" alt="Emmental">
     <div class="card-content">
-        Emmentaler or Emmental is a yellow, medium-hard cheese that originated in the area around Emmental, Switzerland.
-        It is one of the cheeses of Switzerland, and is sometimes known as Swiss cheese.
+        Sairam, Please login with
     </div>
     <div class="card-actions">
-        <paper-button>Share</paper-button>
-        <paper-button>Explore!</paper-button>
+        <google-signin client-id="477636750840-9kmsln38ji1ramlf56uk12ktud92gas6.apps.googleusercontent.com"
+                       scopes="email profile"
+                       on-google-signed-out="_onLogout"
+                       on-google-signin-success="_onLogin" with-backdrop
+                       always-on-top></google-signin>
     </div>
 </paper-card>
 </body>
