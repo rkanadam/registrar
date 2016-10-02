0b85244dfa97883aa1e76ab98c295e2f5c97a050
rkanadam <raghuram.kanadam@gmail.com>
2016-10-02 09:45:01 -0700

    Adding initial login page

--- unauth/login.php
+++ unauth/login.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes">
 
     <script src="../bower_components/webcomponentsjs/webcomponents-lite.js"></script>
-    <link rel="import" href="../bower_components/paper-card/paper-card.html">
-    <link rel="import" href="../bower_components/google-signin/google-signin.html">
+    <link rel="import" href="login-panel.html">
+
 
     <style type="text/css">
         body {
@@ -18,18 +18,6 @@
     </style>
 </head>
 <body>
-
-<paper-card heading="Sign in" alt="Emmental">
-    <div class="card-content">
-        Sairam, Please login with
-    </div>
-    <div class="card-actions">
-        <google-signin client-id="477636750840-9kmsln38ji1ramlf56uk12ktud92gas6.apps.googleusercontent.com"
-                       scopes="email profile"
-                       on-google-signed-out="_onLogout"
-                       on-google-signin-success="_onLogin" with-backdrop
-                       always-on-top></google-signin>
-    </div>
-</paper-card>
+<login-panel></login-panel>
 </body>
 </html>
