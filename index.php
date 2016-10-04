1f414e05e8fbcec9aa068d61b38be45dc9a665b0
rkanadam <raghuram.kanadam@gmail.com>
2016-10-03 22:33:06 -0700

    Fixing layouts

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (!$user->isAuthenticated ()) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes">
 
-    <script src="../bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+    <script src="bower_components/webcomponentsjs/webcomponents-lite.js"></script>
     <link rel="import" href="elements/registrar-app.html">
 
 
