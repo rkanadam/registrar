40743bb5b9ec9b95e3efc75b71dc34c8b1394603
rkanadam <raghuram.kanadam@gmail.com>
2016-10-31 16:09:55 -0700

    Fixing list styling. More changes need to be done.

--- index.php
+++ index.php
@@ -7,16 +7,14 @@ use model\User;
 use util\Log;
 
 $user = User::get();
-
-Log::info("Found the user allright: " . (json_encode($user)));
 if (!$user->isAuthenticated()) {
     header('Location: unauth/login.php');
     return;
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head lang="en">
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes">
@@ -27,15 +25,6 @@ if (!$user->isAuthenticated()) {
     <script src="bower_components/webcomponentsjs/webcomponents-lite.js"></script>
     <script type="text/javascript">window.AppBehaviors = {}</script>
     <link rel="import" href="elements/registrar-app.html">
-
-
-    <style type="text/css">
-        body {
-            margin: 0;
-            font-family: 'Roboto'
-            font-size: 12px;
-        }
-    </style>
 </head>
 <body>
 <registrar-app/>
