35432f473c2877202cc0e6beab7a4e16903af57d
rkanadam <raghuram.kanadam@gmail.com>
2016-11-06 07:07:56 -0800

    Moving things to Big G

---
+++ server/util/Passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace util;
+
+
+class Passwords
+{
+    public static function forDB()
+    {
+        $local = array(
+            '127.0.0.1',
+            '::1'
+        );
+
+        if (false && in_array($_SERVER['REMOTE_ADDR'], $local)) {
+            return ["localhost", "admin", "admin"];
+        } else {
+            return ["rkanadam.cloudant.com", "somporguenesselyncommand", "d992025092e1133221da993faadb63ec8c09487e"];
+        }
+    }
+}
